0d6067d5c7aef45458054f256d5516c1368429c4
alangmartini <77518055+alangmartini@users.noreply.github.com>
2023-01-30 16:56:34 -0400

    Update useFetch.js

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -4,42 +4,40 @@ const useFetch = () => {
   const [isLoading, setIsLoading] = useState(false);
 
   const fetchData = async (URL) => {
-    setIsLoading(true);
     try {
+      setIsLoading(true);
       const response = await fetch(URL, {
         mode: 'cors',
       });
       const json = await response.json();
-      console.log(json);
-      setIsLoading(false);
       return json;
     } catch (error) {
-      throw new Error('Algo deu errado:', error);
+      console.error(error);
     } finally {
       setIsLoading(false);
     }
   };
 
   const getItem = async (endpoint, setStateCallBack, props, id) => {
+    console.log(endpoint);
     // Map props.match.path to variable mealsOrDrinks
     const { type } = props;
-    const { match: { path: mealsOrDrinks } } = props;
-
+    const { history: { location: { pathname: mealsOrDrinks } } } = props;
     // Api a ser usada é 'thecocktailsdb' ou 'themealsdb'
     const APIToUse = type === 'drink' ? 'cocktail' : 'meal';
 
     // const URL = `https://www.the${APIToUse}db.com/api/json/v1/1/search.php?s=`;
     const URL = `https://www.the${APIToUse}db.com/api/json/v1/1/${endpoint}`;
-    console.log(URL);
     const links = await fetchData(URL);
 
-    // links retorna um objeto com chave 'drinks' ou 'meals'.
-    // Pega-se aqui do pathname, que será /drinks ou /meals
-    // e removo o '/'.
     if (id) {
       setStateCallBack(links[mealsOrDrinks.split('/')[0]]);
       return links;
     }
+
+    // links retorna um objeto com chave 'drinks' ou 'meals'.
+    // Pega-se aqui do pathname, que será /drinks ou /meals
+    // e removo o '/'
     setStateCallBack(links[mealsOrDrinks.replace('/', '')]);
     // Retorna o link além do setState para caso onde for usado for fazer alguma outra lógica
     return links;
