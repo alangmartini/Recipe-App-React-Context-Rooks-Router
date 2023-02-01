4e27233531b7dc1010809180977351b93e6d0463
gmaralan <gmartinialan@gmail.com>
2023-02-01 09:49:43 -0400

    added small test to increase coverage

--- src/tests/RecipeDetails.test.js
+++ src/tests/RecipeDetails.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { act } from 'react-dom/test-utils';
+import userEvent from '@testing-library/user-event';
 import renderWithRouterAndProvider from './render/renderWithRouter';
 import fetch from '../../cypress/mocks/fetch';
 import App from '../App';
@@ -10,13 +11,13 @@ describe.only('Testes para RecipeDetails.js', () => {
   beforeEach(async () => {
     global.fetch = jest.fn(fetch);
     const { history } = renderWithRouterAndProvider(<App />);
-    await act(() => history.push('/meals'));
-    const recipeId = await screen.findByTestId(`${0}-recipe-card`);
-    userEvent.click(recipeId);
+    await act(() => history.push('/meals/52771'));
   });
+
   afterEach(() => {
     global.fetch.mockClear();
   });
+
   test('se o elemento foto é renderizado na tela', async () => {
     const photoId = await screen.findByTestId('recipe-photo');
     expect(photoId).toBeInTheDocument();
@@ -32,11 +33,20 @@ describe.only('Testes para RecipeDetails.js', () => {
     expect(categoryId).toBeInTheDocument();
   });
 
-  test('se o elemento ingredientes é renderizado na tela', async () => {
-    const ingId = await screen.findByTestId(`${0}-ingredient-name-and-measure`);
-    expect(ingId).toBeInTheDocument();
-  });
+  //   test('se o elemento categoria é renderizado na tela', async () => {
+  //     const categoryId = await screen.findByTestId('recipe-category');
+  //     expect(categoryId).toBeInTheDocument();
+  //   });
 
+  //   test('se o elemento ingredientes é renderizado na tela', async () => {
+  //     const ingId = await screen.findByTestId(`${index}-ingredient-name-and-measure`);
+  //     expect(ingId).toBeInTheDocument();
+  //   });
+  test('Se é possível clickar em start recipe', () => {
+    const startRecipe = screen.getByTestId('start-recipe-btn');
+
+    userEvent.click(startRecipe);
+  });
   test('se o elemento vídeo é renderizado na tela', async () => {
     const videoId = await screen.findByTestId('video');
     expect(videoId).toBeInTheDocument();
