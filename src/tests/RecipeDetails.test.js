1e0dc7e54fbae08d5603b1517a588d142d2d96a3
gmaralan <gmartinialan@gmail.com>
2023-02-01 09:49:43 -0400

    added small test to increase coverage

--- src/tests/RecipeDetails.test.js
+++ src/tests/RecipeDetails.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { act } from 'react-dom/test-utils';
+import userEvent from '@testing-library/user-event';
 import renderWithRouterAndProvider from './render/renderWithRouter';
 import fetch from '../../cypress/mocks/fetch';
 import App from '../App';
@@ -14,9 +15,11 @@ describe.only('Testes para RecipeDetails.js', () => {
     const recipeId = await screen.findByTestId(`${0}-recipe-card`);
     userEvent.click(recipeId);
   });
+
   afterEach(() => {
     global.fetch.mockClear();
   });
+
   test('se o elemento foto é renderizado na tela', async () => {
     const photoId = await screen.findByTestId('recipe-photo');
     expect(photoId).toBeInTheDocument();
@@ -37,16 +40,15 @@ describe.only('Testes para RecipeDetails.js', () => {
     expect(instructionsId).toBeInTheDocument();
   });
 
-  // test('se o elemento ingredientes é renderizado na tela', async () => {
-  //   const ingId = await screen.findByTestId(`${index}-ingredient-name-and-measure`);
-  //   expect(ingId).toBeInTheDocument();
-  // });
+  test('se o elemento ingredientes é renderizado na tela', async () => {
+    const ingId = await screen.findByTestId(`${index}-ingredient-name-and-measure`);
+    expect(ingId).toBeInTheDocument();
+  });
 
   test('Se é possível clickar em start recipe', () => {
     const startRecipe = screen.getByTestId('start-recipe-btn');
     userEvent.click(startRecipe);
   });
-
   test('se o elemento vídeo é renderizado na tela', async () => {
     const videoId = await screen.findByTestId('video');
     expect(videoId).toBeInTheDocument();
