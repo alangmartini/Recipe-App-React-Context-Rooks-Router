18542634fac17981656ea4db0d5c14bbe4186da2
Rusty <gralbergs@gmail.com>
2023-02-01 19:24:10 -0300

    teste sem erro de lint

--- src/tests/DoneRecipes.test.js
+++ src/tests/DoneRecipes.test.js
@@ -1,105 +1,26 @@
-import React from 'react';
-import { screen } from '@testing-library/react';
+import { act, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import React from 'react';
+import oneDrink from '../../cypress/mocks/oneDrink';
 import App from '../App';
-import { renderWithRouter } from './render/renderWithRouter';
+import renderWithRouter from './helpers/renderWithRouter';
 
-const PROFILE_TOP_BTN = 'profile-top-btn';
-const PROFILE_DONE_BTN = 'profile-done-btn';
+const ONE_DRINK = '/drinks/178319/in-progress';
 
-const mockLocalStorage = [{
-  id: '52977',
-  type: 'meal',
-  nationality: 'Turkish',
-  category: 'Side',
-  alcoholicOrNot: '',
-  name: 'Corba',
-  image: 'https://www.themealdb.com/images/media/meals/58oia61564916529.jpg',
-  doneDate: '17/03/2022',
-  tags: ['Soup'],
-},
-{
-  id: '52785',
-  type: 'meal',
-  nationality: 'Indian',
-  category: 'Vegetarian',
-  alcoholicOrNot: '',
-  name: 'Dal fry',
-  image: 'https://www.themealdb.com/images/media/meals/wuxrtu1483564410.jpg',
-  doneDate: '04/02/2022',
-  tags: ['Curry', 'Vegetarian'],
-},
-{
-  id: '15997',
-  type: 'drink',
-  nationality: '',
-  category: 'Ordinary Drink',
-  alcoholicOrNot: 'Optional alcohol',
-  name: 'GG',
-  image: 'https://www.thecocktaildb.com/images/media/drink/vyxwut1468875960.jpg',
-  doneDate: '15/04/2022',
-  tags: [],
-}];
-
-const initialEntries = ['/done-recipes'];
-
-describe('Testa a página Done Recipes', () => {
-  beforeEach(() => {
-    window.localStorage.setItem('doneRecipes', JSON.stringify(mockLocalStorage));
-  });
-
-  afterEach(() => window.localStorage.clear());
-
-  it('Verifica rota até a página Done Recipes, renderização da página e elementos e click em filter by meal .', async () => {
-    renderWithRouter(<App />, { initialEntries });
-    const inputEmail = screen.getByTestId('email-input');
-    expect(inputEmail).toBeInTheDocument();
-
-    const inputPassword = screen.getByTestId('password-input');
-    expect(inputPassword).toBeInTheDocument();
-
-    const loginBtn = screen.getByRole('button', { name: /entrar/i });
-    expect(loginBtn).toBeDisabled();
-
-    userEvent.type(inputEmail, 'teste@test.com');
-    userEvent.type(inputPassword, '1234567');
-    expect(loginBtn).toBeEnabled();
-    userEvent.click(loginBtn);
-
-    expect(screen.getByTestId(PROFILE_TOP_BTN)).toBeInTheDocument();
-    userEvent.click(screen.getByTestId(PROFILE_TOP_BTN));
-    expect(screen.getByTestId(PROFILE_DONE_BTN)).toBeInTheDocument();
-    userEvent.click(screen.getByTestId(PROFILE_DONE_BTN));
-
-    expect(screen.getByRole('button', { name: /all/i })).toBeInTheDocument();
-    expect(screen.getByRole('button', { name: /meals/i })).toBeInTheDocument();
-    expect(screen.getByRole('button', { name: /drinks/i })).toBeInTheDocument();
-
-    expect(screen.getByTestId('0-horizontal-name')).toBeInTheDocument();
-    expect(screen.getByTestId('0-Soup-horizontal-tag')).toBeInTheDocument();
-
-    expect(screen.getByTestId('filter-by-meal-btn')).toBeInTheDocument();
-    userEvent.click(screen.getByTestId('filter-by-meal-btn'));
-  });
-
-  it('Checa a função click filter by drink.', async () => {
-    renderWithRouter(<App />, { initialEntries });
-
-    expect(screen.getByTestId('filter-by-drink-btn')).toBeInTheDocument();
-    userEvent.click(screen.getByTestId('filter-by-drink-btn'));
-  });
-
-  it('Checa a função click filter by all. .', async () => {
-    renderWithRouter(<App />, { initialEntries });
-
-    expect(screen.getByTestId('filter-by-all-btn')).toBeInTheDocument();
-    userEvent.click(screen.getByTestId('filter-by-all-btn'));
-  });
-
-  it('Verifica a função click do botão, que faz redirecionamento de página (history.push(path)).', async () => {
-    renderWithRouter(<App />, { initialEntries });
-
-    expect(screen.getByTestId('0-horizontal-image')).toBeInTheDocument();
-    userEvent.click(screen.getByTestId('0-horizontal-image'));
+describe('Teste da tela de recipes terminados', () => {
+  it('Testar se os elementos aparecem na tela', async () => {
+    global.fetch = jest.fn().mockResolvedValue({
+      json: jest.fn().mockResolvedValue(oneDrink),
+    });
+    const { history } = renderWithRouter(<App />);
+    await act(async () => {
+      history.push(ONE_DRINK);
+    });
+    userEvent.click(screen.getByText(/2 oz hpnotiq/i));
+    userEvent.click(screen.getByText(/1 oz pineapple juice/i));
+    userEvent.click(screen.getByText(/1 oz banana liqueur/i));
+    expect(screen.getByRole('button', { name: /finalizar/i })).toBeEnabled();
+    userEvent.click(screen.getByRole('button', { name: /finalizar/i }));
+    expect(history.location.pathname).toBe('/done-recipes');
   });
 });
